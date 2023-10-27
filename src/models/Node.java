b32aa7de113856706ff06f457aa36329e9ed21a8
tofylion <tofy2001@hotmail.com>
2023-10-28 01:18:23 +0200

    Created temp node class

---
+++ src/models/Node.java
@@ -0,0 +1,13 @@
+package models;
+
+public class Node {
+    public Node() {
+        //TODO: constructor body
+    }
+
+    public String getPath() {
+        //TODO: implement getPath
+        // Return is of the following format: plan;monetaryCost;nodesExpanded
+        throw new UnsupportedOperationException();
+    }
+}
