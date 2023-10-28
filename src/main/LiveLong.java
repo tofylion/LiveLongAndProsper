d3e5aee0a0da1613e1ab5cfcc40ba6a2443b60cc
Mohamed El-Alafty <96385365+alafty@users.noreply.github.com>
2023-10-28 21:11:00 +0200

    created the LiveLong class

---
+++ src/main/LiveLong.java
@@ -0,0 +1,10 @@
+package main;
+
+import interfaces.SearchStrategy;
+
+public class LiveLong extends GenericSearch {
+
+    public String solve(String initialState, SearchStrategy strategy, boolean vizualize) {
+        return super.solve(initialState, strategy, vizualize);
+    }
+}
