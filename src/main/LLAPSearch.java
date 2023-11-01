03e3a2725821350c1a14c9e2e1244b9aa35b6514
Mohamed El-Alafty <96385365+alafty@users.noreply.github.com>
2023-11-01 18:53:47 +0200

    updated the class name as requested

--- src/main/LLAPSearch.java
+++ src/main/LLAPSearch.java
@@ -2,7 +2,7 @@ package main;
 
 import interfaces.SearchStrategy;
 
-public class LiveLong extends GenericSearch {
+public class LLAPSearch extends GenericSearch {
 
     public String solve(String initialState, SearchStrategy strategy, boolean vizualize) {
         return super.solve(initialState, strategy, vizualize);
