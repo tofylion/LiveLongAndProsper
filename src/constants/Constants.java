0c104a2468929a83127029745531f24fdca01fa1
Mohamed El-Alafty <96385365+alafty@users.noreply.github.com>
2023-11-08 18:23:05 +0200

    removed the getProsperitygoal and its dependencies

--- src/constants/Constants.java
+++ src/constants/Constants.java
@@ -3,11 +3,6 @@ public class Constants {
     public static int budget = 100000;
     public static int resourceLimit = 50;
     public static int prosperityGoal = 100;
-    
-    public int getProsperityGoal()
-    {
-        return prosperityGoal;
-    }
 }
 
     
