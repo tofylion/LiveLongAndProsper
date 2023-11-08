ebde3bace0a75bf97a62f169171f3f7cdcc1242d
Mohamed El-Alafty <96385365+alafty@users.noreply.github.com>
2023-11-08 18:23:26 +0200

    renamed A* to greedy

---
+++ src/strategies/GreedySearch.java
@@ -0,0 +1,47 @@
+package strategies;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import models.Node;
+import models.NodeByBuild;
+import interfaces.BestFirstSearch;
+
+public class GreedySearch implements BestFirstSearch  {
+
+    public Comparator<Node> firstHeuristic;
+    public Comparator<Node> secondHeuristic;
+
+    public GreedySearch(Comparator<Node> firstHeuristic, Comparator<Node> secondHeuristic){
+        this.firstHeuristic = firstHeuristic;
+        this.secondHeuristic = secondHeuristic;
+    }
+
+    @Override
+    public Queue<Node> queueingFunction(Queue<Node> nodes, Node[] newNodes) {
+        PriorityQueue<Node> queue = new PriorityQueue<Node>(nodes.size() + newNodes.length, firstHeuristic);
+        //add the existing nodes to the PQ
+        for (Node node: nodes){
+            queue.add(node);
+        }
+        //add the new nodes to the PQ
+        for (Node node: newNodes){
+            queue.add(node);
+        }
+        //typecast the PQ to a normal queue
+        Queue<Node> returnQueue = queue;
+        return returnQueue;
+
+    }
+
+    @Override
+    public Queue<Node> firstHeuristic(Queue<Node> nodes, Node[] newNodes) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'firstHeuristic'");
+    }
+
+    @Override
+    public Queue<Node> secondHeuristic(Queue<Node> nodes, Node[] newNodes) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'secondHeuristic'");
+    }
+}
